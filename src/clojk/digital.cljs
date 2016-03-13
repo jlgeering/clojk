b5239f84c14cde62292a34b9ee4dc69bfa8d9251
Jean-Luc Geering <jlgeering.13@gmail.com>
2016-03-13 18:18:08 +0100

    refactor

--- src/clojk/digital.cljs
+++ src/clojk/digital.cljs
@@ -4,8 +4,8 @@
   (:require-macros
    [devcards.core :as dc :refer [defcard deftest]]))
 
-(defn seven-segment-display [class segment-classes segment-default]
-  [:g {:class class}
+(defn seven-segment-display [segment-classes segment-default]
+  [:g {:class "segments"}
    [:path {:class ["segment" "segment-a" (:a segment-classes segment-default)] :d "M10,8L14,4L42,4L46,8L42,12L14,12L10,8z"}]
    [:path {:class ["segment" "segment-b" (:b segment-classes segment-default)] :d "M48,10L52,14L52,42L48,46L44,42L44,14L48,10z"}]
    [:path {:class ["segment" "segment-c" (:c segment-classes segment-default)] :d "M48,50L52,54L52,82L48,86L44,82L44,54L48,50z"}]
@@ -14,8 +14,8 @@
    [:path {:class ["segment" "segment-f" (:f segment-classes segment-default)] :d "M8,10L12,14L12,42L8,46L4,42L4,14L8,10z"}]
    [:path {:class ["segment" "segment-g" (:g segment-classes segment-default)] :d "M10,48L14,44L42,44L46,48L42,52L14,52L10,48z"}]])
 
-(defn separator [class]
-  [:g {:class class}
+(defn separator [separator-classes]
+  [:g {:class ["separator" separator-classes]}
    [:circle {:r 4 :cx 0 :cy 28}]
    [:circle {:r 4 :cx 0 :cy 68}]])
 
@@ -31,43 +31,51 @@
    \9 {:a "lit" :b "lit" :c "lit" :d "lit" :e "off" :f "lit" :g "lit"}
    \0 {:a "lit" :b "lit" :c "lit" :d "lit" :e "lit" :f "lit" :g "off"}})
 
-(defn digit-display [class digit]
-  (seven-segment-display class (get digit-to-classes digit) nil))
+(defn digit-display [digit]
+  (seven-segment-display (get digit-to-classes digit) nil))
+
+(defn digits-display [digits]
+  (map-indexed
+   (fn [idx itm]
+     [:g {:key idx :transform (str "translate(" (* 60 idx) ",0)")}
+      (digit-display itm)])
+   (str digits)))
+
+; ----------------------------------------------------
 
 (defcard seven-segment-display
   (sab/html [:div {:class "segment-display"}
              [:svg {:width 375 :height 96 :viewBox "0 0 375 96"}
               [:g {:transform "translate(0,0)"}
-               (seven-segment-display "ssd" {} "off")
-               [:g {:transform "translate( 60,0)"} (separator "separator off")]]]]))
+               (seven-segment-display {} "off")
+               [:g {:transform "translate( 60,0)"} (separator "off")]]]]))
 
 (defcard seven-segment-display-lit
   (sab/html [:div {:class "segment-display"}
              [:svg {:width 375 :height 96 :viewBox "0 0 375 96"}
               [:g {:transform "translate(0,0)"}
-               (seven-segment-display "ssd" {} "lit")
-               [:g {:transform "translate( 60,0)"} (separator "separator lit")]]]]))
+               (seven-segment-display {} "lit")
+               [:g {:transform "translate( 60,0)"} (separator "lit")]]]]))
 
 (defcard digits
   (sab/html [:div {:class "segment-display"}
              [:svg {:width 600 :height 96 :viewBox "0 0 600 96"}
-              [:g {:transform "translate(  0,0)"} (digit-display "ssd" \1)]
-              [:g {:transform "translate( 60,0)"} (digit-display "ssd" \2)]
-              [:g {:transform "translate(120,0)"} (digit-display "ssd" \3)]
-              [:g {:transform "translate(180,0)"} (digit-display "ssd" \4)]
-              [:g {:transform "translate(240,0)"} (digit-display "ssd" \5)]
-              [:g {:transform "translate(300,0)"} (digit-display "ssd" \6)]
-              [:g {:transform "translate(360,0)"} (digit-display "ssd" \7)]
-              [:g {:transform "translate(420,0)"} (digit-display "ssd" \8)]
-              [:g {:transform "translate(480,0)"} (digit-display "ssd" \9)]
-              [:g {:transform "translate(540,0)"} (digit-display "ssd" \0)]]]))
+              [:g {:transform "translate(  0,0)"} (digit-display \1)]
+              [:g {:transform "translate( 60,0)"} (digit-display \2)]
+              [:g {:transform "translate(120,0)"} (digit-display \3)]
+              [:g {:transform "translate(180,0)"} (digit-display \4)]
+              [:g {:transform "translate(240,0)"} (digit-display \5)]
+              [:g {:transform "translate(300,0)"} (digit-display \6)]
+              [:g {:transform "translate(360,0)"} (digit-display \7)]
+              [:g {:transform "translate(420,0)"} (digit-display \8)]
+              [:g {:transform "translate(480,0)"} (digit-display \9)]
+              [:g {:transform "translate(540,0)"} (digit-display \0)]]]))
 
 (defcard number-strings
   (sab/html [:div {:class "segment-display"}
              [:svg {:width 600 :height 96 :viewBox "0 0 600 96"}
-              (map-indexed
-               (fn [idx itm] [:g {:key idx :transform (str "translate(" (* 60 idx) ",0)")} (digit-display "ssd" itm)])
-               (str "0123456789"))]]))
+              (digits-display "0123456789")]]))
+
 
 ; <svg >
 ;   <g transform="translate(17,0)">
