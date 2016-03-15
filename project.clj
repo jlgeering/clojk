5a180a5ffcbd90a3a721089e0067fef9e3821488
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2016-03-15 12:45:58 +0100

    upgrade to clojure 1.8.0

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
   :min-lein-version "2.5.3"
-  
-  :dependencies [[org.clojure/clojure "1.7.0"]
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [devcards "0.2.1-6"]
                  [sablono "0.6.2"]
-                 
+
                  ;; need to specify this for sablono
                  ;; when not using devcards
                  [cljsjs/react "0.14.3-0"]
@@ -19,24 +19,24 @@
 
                  #_[org.omcljs/om "0.9.0"]
                  #_[reagent "0.5.1"]]
-  
+
   :plugins [[lein-figwheel "0.5.0-6"]
             [lein-cljsbuild "1.1.2" :exclusions [org.clojure/clojure]]]
 
   :clean-targets ^{:protect false} ["resources/public/js/compiled"
                                     "target"]
-  
+
   :source-paths ["src"]
 
   :cljsbuild {
               :builds [{:id "devcards"
                         :source-paths ["src"]
-                        :figwheel { :devcards true } ;; <- note this
+                        :figwheel { :devcards true} ;; <- note this
                         :compiler { :main       "clojk.core"
                                     :asset-path "js/compiled/devcards_out"
                                     :output-to  "resources/public/js/compiled/clojk_devcards.js"
                                     :output-dir "resources/public/js/compiled/devcards_out"
-                                    :source-map-timestamp true }}
+                                    :source-map-timestamp true}}
                        {:id "dev"
                         :source-paths ["src"]
                         :figwheel true
@@ -44,7 +44,7 @@
                                    :asset-path "js/compiled/out"
                                    :output-to  "resources/public/js/compiled/clojk.js"
                                    :output-dir "resources/public/js/compiled/out"
-                                   :source-map-timestamp true }}
+                                   :source-map-timestamp true}}
                        {:id "prod"
                         :source-paths ["src"]
                         :compiler {:main       "clojk.core"
@@ -52,4 +52,4 @@
                                    :output-to  "resources/public/js/compiled/clojk.js"
                                    :optimizations :advanced}}]}
 
-  :figwheel { :css-dirs ["resources/public/css"] })
+  :figwheel { :css-dirs ["resources/public/css"]})
