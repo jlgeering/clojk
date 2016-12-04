0fedd4c90ab0d262a2ff15c1f782f6b6277fd124
Jean-Luc Geering <jlgeering.13@gmail.com>
2016-12-04 21:34:21 +0100

    use atom for current time

--- src/clojk/tz.cljs
+++ src/clojk/tz.cljs
@@ -6,18 +6,34 @@
    [devcards.core :as dc :refer [defcard deftest]]))
 
 (defcard show-time
-  (sab/html [:div {:class "time"}
+  (sab/html [:div [:div {:class "time"}
+             [:span {:class "hours"} "01"]
+             [:span ":"]
+             [:span {:class "minutes"} "01"]
+             [:span ":"]
+             [:span {:class "seconds"} "01"]]
+            [:div {:class "time"}
              [:span {:class "hours"} "12"]
              [:span ":"]
              [:span {:class "minutes"} "34"]
              [:span ":"]
-             [:span {:class "seconds"} "56"]]))
+             [:span {:class "seconds"} "56"]]]))
+
+(defonce state
+  (let [a (atom {:time 0})]
+    (js/setInterval (fn [] (swap! state update-in [:time] t/now)) 200)
+    a))
+
+(defcard state-observer state {} {:history false})
 
 (defcard current-time
-  (sab/html (let [now (t/now)]
-              [:div {:class "time"}
-               [:span {:class "hours"} (t/hour now)]
-               [:span ":"]
-               [:span {:class "minutes"} (t/minute now)]
-               [:span ":"]
-               [:span {:class "seconds"} (t/second now)]])))
+  (fn [data-atom _]
+    (sab/html (let [now (:time @data-atom)]
+                [:div {:class "time"}
+                 [:span {:class "hours"} (t/hour now)]
+                 [:span ":"]
+                 [:span {:class "minutes"} (t/minute now)]
+                 [:span ":"]
+                 [:span {:class "seconds"} (t/second now)]])))
+  state
+  {:inspect-data true})
