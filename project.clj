1952f80864b91dd5e1b49a93ab68953ebc330d88
Jean-Luc Geering <jlgeering.13@gmail.com>
2016-12-29 08:21:01 +0100

    min lein version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :min-lein-version "2.5.3"
+  :min-lein-version "2.7.1"
 
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.9.293"]
