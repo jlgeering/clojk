20a8237b444dc3386a4f5454f2a104be905ee480
Jean-Luc Geering <jeanluc.geering@ufirstgroup.com>
2019-04-10 08:22:22 +0200

    snapshot

--- project.clj
+++ project.clj
@@ -78,4 +78,78 @@
   ; :aliases {"build-docs" ["do" "clean" ["cljsbuild" "once" "docs-index"] ["cljsbuild" "once" "docs-devcards"]]}
   :aliases {"build-docs" ["cljsbuild" "once" "docs-index" "docs-devcards"]}
   
+  ; :cljsbuild {:builds {:app
+  ;                      {:source-paths ["src" "env/dev/cljs"]
+  ;                       :compiler
+  ;                       {:main "reagent-frontend-demo.dev"
+  ;                        :output-to "public/js/app.js"
+  ;                        :output-dir "public/js/out"
+  ;                        :asset-path   "js/out"
+  ;                        :source-map true
+  ;                        :optimizations :none
+  ;                        :pretty-print  true}}
+  ;                      :release
+  ;                      {:source-paths ["src" "env/prod/cljs"]
+  ;                       :compiler
+  ;                       {:output-to "public/js/app.js"
+  ;                        :output-dir "public/js/release"
+  ;                        :asset-path   "js/out"
+  ;                        :optimizations :advanced
+  ;                        :pretty-print false}}}}
+
+  ; https://github.com/emezeske/lein-cljsbuild/blob/1.1.5/sample.project.clj
+  ; :cljsbuild {
+  ;   :builds [{
+  ;       ; The path to the top-level ClojureScript source directory:
+  ;       :source-paths ["src-cljs"]
+  ;       ; The standard ClojureScript compiler options:
+  ;       ; (See the ClojureScript compiler documentation for details.)
+  ;       :compiler {
+  ;         :output-to "war/javascripts/main.js"  ; default: target/cljsbuild-main.js
+  ;         :optimizations :whitespace
+  ;         :pretty-print true}}]}
+
+  ; :figwheel {:http-server-root "public"
+  ;            :nrepl-port 7002
+  ;            :nrepl-middleware ["cemerick.piggieback/wrap-cljs-repl"]
+  ;            :css-dirs ["public/css"]}
+  ; :figwheel {;; :http-server-root "public" ;; default and assumes "resources"
+  ;            ;; :server-port 3449 ;; default
+  ;            ;; :server-ip "127.0.0.1"
+  ; 
+  ;            :css-dirs ["resources/public/css"] ;; watch and update CSS
+  ; 
+  ;            ;; Start an nREPL server into the running figwheel process
+  ;            ;; :nrepl-port 7888
+  ; 
+  ;            ;; Server Ring Handler (optional)
+  ;            ;; if you want to embed a ring handler into the figwheel http-kit
+  ;            ;; server, this is for simple ring servers, if this
+  ; 
+  ;            ;; doesn't work for you just run your own server :) (see lein-ring)
+  ; 
+  ;            ;; :ring-handler hello_world.server/handler
+  ; 
+  ;            ;; To be able to open files in your editor from the heads up display
+  ;            ;; you will need to put a script on your path.
+  ;            ;; that script will have to take a file path and a line number
+  ;            ;; ie. in  ~/bin/myfile-opener
+  ;            ;; #! /bin/sh
+  ;            ;; emacsclient -n +$2 $1
+  ;            ;;
+  ;            ;; :open-file-command "myfile-opener"
+  ; 
+  ;            ;; if you are using emacsclient you can just use
+  ;            ;; :open-file-command "emacsclient"
+  ; 
+  ;            ;; if you want to disable the REPL
+  ;            ;; :repl false
+  ; 
+  ;            ;; to configure a different figwheel logfile path
+  ;            ;; :server-logfile "tmp/logs/figwheel-logfile.log"
+  ;            }
+
+  ; :profiles {:dev {:dependencies [[figwheel-sidecar "0.5.8"]
+  ;                                 [org.clojure/tools.nrepl "0.2.12"]
+  ;                                 [com.cemerick/piggieback "0.2.1"]]}}
   )
